3fa0b9dfa7aacdd24ed19a5efbefed37773b7b9e
Didier Alzate <didierltz@gmail.com>
2024-07-05 21:02:58 -0500

    Se Genera la funcion en vistas y la url para poder tener un href en el home que lleve al index

--- pages/urls.py
+++ pages/urls.py
@@ -3,6 +3,8 @@
 from django.urls import path
 from pages import views
 
+
 urlpatterns = [
     path("", views.home, name='home'),
+    path("index", views.Curso_index, name="curso_index"), # intento crear un href en home para ir a index
 ]
--- pages/views.py
+++ pages/views.py
@@ -1,8 +1,22 @@
 # pages/views.py
 
 from django.shortcuts import render
+from cursos.models import Curso
 
 # Create your views here.
 
 def home(request):
     return render(request, "pages/home.html", {})
+
+
+# intento crear un href en home para ir a index
+# para lograrlo 1 creo la funcion  Curso_index en views
+
+def Curso_index(request):
+    Cursos = Curso.objects.all()
+    context = {
+        "Cursos": Cursos
+    }
+    return render(request, "cursos/curso_index.html", context)
+
+
