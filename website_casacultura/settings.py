6424dbbe4282bed6affd81b34dcc063b5dcc4c04
Didier Alzate <didierltz@gmail.com>
2024-07-19 00:43:36 -0500

    se mejora estlo de home, se genera styles css y se comprueba que se puede referenciar desde base.html

--- website_casacultura/settings.py
+++ website_casacultura/settings.py
@@ -122,7 +122,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR /"var\www"
+STATIC_ROOT = BASE_DIR /"var/www"
 
 STATICFILES_DIRS = [
     BASE_DIR / "static",
